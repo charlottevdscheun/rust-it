70fd2fbb6507052bbde9eea3cf27635f73bab47c
Charlotte van der Scheun <charlotte.van.der.scheun@ah.nl>
2023-04-11 09:12:25 +0200

    dead code allow

--- hello.rs
+++ hello.rs
@@ -1,9 +1,19 @@
 fn main(){
-    // Make a comment
-    
+    // things learned:
     /*
-    this is all a comment
+    - Rust does macros with !
+    - the ; is back after every line
+    - 
+
     */
+
     let x = 5* 6 /* comment again */ + 1;
     println!("Hi there, x is = {}", x);
+    format!("this is a string");
+    print!("this is a print to console");
+    eprint!("this is an error");
+
+    #[allow(dead_code)]  // this disabled the warning for deadcode
+    struct Structure(i32);
+
 }
